08c7bb32e6d0bf1c05a622373752a5e96166db26
wintowasted <84101286+wintowasted@users.noreply.github.com>
2022-02-16 16:02:36 +0300

    Button click update
    Updated the warning message for button click while edit text is empty

--- app/src/main/java/com/example/shortly/MainActivity.kt
+++ app/src/main/java/com/example/shortly/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.shortly
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.example.shortly.databinding.ActivityMainBinding
+
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.apply {
+            shortenButton.setOnClickListener {
+
+                if (editLink.getText().toString().equals("")) {
+                    editLink.setHint(getString(R.string.empty_edit))
+                    editLink.setTextSize(17F)
+                    editLink.setHintTextColor(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.edit_link
+                        )
+                    )
+                    editLink.setBackgroundResource(R.drawable.ic_empty_edit)
+                }
+
+
+
+            }
+
+            editLink.onFocusChangeListener  = View.OnFocusChangeListener { view, hasFocus ->
+                if (hasFocus){
+                    editLink.setHint("")
+                    editLink.setBackgroundResource(R.drawable.ic_edit_vector)
+                }
+            }
+
+            val keyListener = View.OnKeyListener { p0, p1, p2 ->
+                if(p2.action == KeyEvent.ACTION_DOWN && p1 == KeyEvent.KEYCODE_ENTER){
+                    hideSoftKeyboard()
+                    editLink.clearFocus()
+                    true
+                }else{
+                    false
+                }
+            }
+            editLink.setOnKeyListener(keyListener)
+        }
+    }
+
+    fun Activity.hideSoftKeyboard(){
+        (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager).apply {
+            hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
     }
 }
